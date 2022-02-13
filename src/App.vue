33398a4a71905ee5c66bc4b247638e33c92d6fb1
vin <vin@cybridge.jp>
2022-02-13 11:42:10 +0700

    update coding

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p v-for="(item, index) in data" :key="index">
+      {{ item }}
+      <button @click="handleDelete(item)">Delete</button>
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      data: ['Cam', 'Buoi', 'Xoai']
+    }
+  },
+  methods: {
+    handleDelete(i) {
+      this.data = this.data.filter((e) => e !== i )
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <p v-if="isShow">{{ title }}</p>
+      <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Todo',
+    data() {
+        return {
+            isShow: true,
+            title: 'Hello world'
+        }
+    },
+    methods: {
+        handleClick() {
+            this.isShow = !this.isShow;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
